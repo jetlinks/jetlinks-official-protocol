6e4ff8e32f283180f120fb3790ded8f6e8d87334
zhou-hao <admin@hsweb.me>
2021-07-19 11:39:04 +0800

    NoArgsConstructor

--- src/main/java/org/jetlinks/protocol/official/TopicPayload.java
+++ src/main/java/org/jetlinks/protocol/official/TopicPayload.java
@@ -2,11 +2,13 @@ package org.jetlinks.protocol.official;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor(staticName = "of") // FIXME 使用对象池,节省一丢丢内存?
+@AllArgsConstructor(staticName = "of")
+@NoArgsConstructor
 public class TopicPayload {
 
     private String topic;
