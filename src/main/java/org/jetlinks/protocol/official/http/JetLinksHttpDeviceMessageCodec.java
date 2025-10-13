043c1a155c0456aeab15878ce731d9d20c8d6c4c
gyl <1554863190@qq.com>
2025-10-13 17:35:12 +0800

    feat: 优化

--- src/main/java/org/jetlinks/protocol/official/http/JetLinksHttpDeviceMessageCodec.java
+++ src/main/java/org/jetlinks/protocol/official/http/JetLinksHttpDeviceMessageCodec.java
@@ -51,7 +51,6 @@ import java.util.Objects;
 public class JetLinksHttpDeviceMessageCodec extends BlockingDeviceMessageCodec implements Authenticator {
 
     static final ConfigKey<String> BEARER_TOKEN = ConfigKey.of("bearer_token");
-    static final ConfigKey<String> WS_TOKEN = ConfigKey.of("ws_token");
 
     public static final DefaultConfigMetadata httpConfig = new DefaultConfigMetadata(
         "HTTP认证配置"
@@ -61,7 +60,7 @@ public class JetLinksHttpDeviceMessageCodec extends BlockingDeviceMessageCodec i
     public static final DefaultConfigMetadata webSocketConfig = new DefaultConfigMetadata(
         "WebSocket认证配置"
         , "使用WebSocket param进行认证")
-        .add(WS_TOKEN.getKey(), "token", "连接携带token参数", new PasswordType());
+        .add(BEARER_TOKEN.getKey(), "token", "连接携带token参数", new PasswordType());
 
     public JetLinksHttpDeviceMessageCodec(ServiceContext context, Transport transport) {
         super(context, transport);
